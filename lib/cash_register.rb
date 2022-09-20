f204a2555f398b77ba7a258da2fca04749212e6a
Shelby Lee <slee337@uw.edu>
2022-09-20 16:34:58 -0600

    commit tree eb72e859e16ab96df2973675b7fa7c614bef088d

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,44 @@
+class CashRegister
 
+    attr_reader :discount
+
+    attr_accessor :total, :last_transaction, :items
+
+    def initialize(discount = 0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+
+    def add_item(title, price, quantity = 1)
+        self.last_transaction = price*quantity
+        self.total += (price * quantity)
+        quantity.times do
+            self.items << title
+        end
+    end
+
+    def apply_discount
+        if discount > 0
+            self.total -= ((total*discount)/100)
+            "After the discount, the total comes to $#{total}."
+        else
+            "There is no discount to apply."
+        end
+    end
+
+    def void_last_transaction
+        self.total -= last_transaction
+    end
+
+
+
+
+
+
+
+
+
+
+
+end
